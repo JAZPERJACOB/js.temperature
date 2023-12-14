5538dfe288959c2cc65bc04638268b150d7fcd43
JAZPERJACOB <151716533+JAZPERJACOB@users.noreply.github.com>
2023-12-13 19:08:19 -0800

    Add files via upload

---
+++ Temperature/scripts/practice.js
@@ -0,0 +1,136 @@
+console.log("Practice")
+
+//Jazper's Info
+let studentName="Jazper";
+let myAge=29;
+let aStudent= true;
+
+//display the values
+console.log("My name is" + studentName);
+console.log("My age is" + myAge);
+console.log("Active" + aStudent);
+
+let num1=10;
+let num2=5;
+
+console.log("Multiplication: " + num1*num2);
+console.log("Addition:" + num1+num2);
+console.log("Sub:" + num1-num2);
+console.log("Div:" + num1/num2)
+
+let assignmentName = "Class 1 - Assingment 1";
+let description= "Assignments are due the day Session 3 is held by 11:59 p.m. (in the time zone in which each student resides). Assignments represent 20% of the overall course grade.";
+let pointsAs1 = 6.66;
+let pointsAs2 = 6.66;
+let pointsAs3 = 6.66;
+
+let total = pointsAs1 + pointsAs2 + pointsAs3;
+let fileUploads= true;
+let studentAnnotation = false;
+
+let dueDate="12/20/2023";
+
+document.write(`<h1> ${assignmentName} </h1>`);
+document.write(`<p> ${description} </p>`);
+
+//assigntment 1 (103)
+
+let profileName="Japzer";
+profileName="Louie";
+profileName="Alyssa";
+profileName="Alex";
+
+let menu="Search";
+menu="Home";
+menu="New & Popular";
+menu="TV Shows";
+mernu="Movies";
+
+let categories="Action";
+categories="Comedy";
+categories="Horror";
+categories="Romance";
+
+let trendingNow="Suits";
+trendingNow="World War2";
+trendingNow="8 Mile";
+
+let top10MoviesinTheusToday="Mario Bros.";
+top10MoviesinTheusToday="Love and Monsters";
+top10MoviesinTheusToday="Suicide Squad";
+top10MoviesinTheusToday="Shark boy & Lava girl";
+
+let newReleases="Headliners Only";
+newReleases="Love and Monsters";
+newReleases="Car Masters";
+
+let criticallyAcclaimedmovies="Boyz n the Hood";
+criticallyAcclaimedmovies="Silver Linings Playbook";
+criticallyAcclaimedmovies="Catch me if you can";
+
+let topSearches="Taken";
+topSearches="Hunter Killer";
+topSearches="Prometheus";
+
+let blockbusterMovies="Wonder Woman";
+blockbusterMovies="The Meg";
+blockbusterMovies="6 Underground";
+
+let popularOnnetflix="Silent Hill";
+popularOnnetflix="Top Chef";
+popularOnnetflix="Shadow and Bones";
+
+let comingSoon="Yu Yu Hakasho";
+comingSoon="Gran Turismo";
+comingSoon="Rebel Moon";
+
+let actionMovies="Robin Hood";
+actionMovies="Elysium";
+actionMovies="Real Steel";
+
+let onlyOnnetflix="Holidate";
+onlyOnnetflix="Community";
+onlyOnnetflix="Ted 2"
+
+let romanticMovies="Love Hard";
+romanticMovies="The Bounty Hunter";
+romanticMovies="A Nice Girl like you";
+
+let youngAdultmovies="Legacies";
+youngAdultmovies="Last Chance U";
+youngAdultmovies="Outer Banks";
+
+let documentaries="Our Universe";
+documentaries="Catching Killers";
+documentaries="High Score";
+
+let trueCrimedocuseries="Bad Sport";
+trueCrimedocuseries="The Puppet Master";
+trueCrimedocuseries="Dirty Money";
+
+let familiarFavorites="Kung Fu Panda";
+familiarFavorites="2012";
+familiarFavorites="This is the End";
+
+let casualViewing="Key & Peele";
+casualViewing="Forged in Fire";
+casualViewing="The Final Table";
+
+let recentlyAdded="99 Homes";
+recentlyAdded="Black Swan";
+recentlyAdded="Criminal Code";
+
+let retroTv="Justice League";
+retroTv="Drake & Josh";
+retroTv="NCIS";
+
+let familyAnimation="Sing 2";
+familyAnimation="Tintin";
+familyAnimation="minions";
+
+
+
+
+
+
+
